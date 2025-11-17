d6c5b366549e712b37436d8670d23846ef7a6246
zadumka <90329284+zadumka@users.noreply.github.com>
2025-11-17 19:33:36 +0200

    Refactor filterArray to use for loop
    Refactor filterArray function to use a for loop instead of forEach.

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,12 @@
 function filterArray(numbers, value) {
-const filteredArray = [];
-  numbers.forEach(elem => {
-    elem > value ? filteredArray.push(elem) : null;
-  });
+ const newArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
+  return newArray;
+}
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
