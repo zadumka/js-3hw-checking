2a14ec626c9c1970426639fbc117208632efdd6b
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-15 12:30:24 +0200

    Replace filterArray function with getElementWidth

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,11 @@
-function filterArray(numbers, value) {
- const newArray = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      newArray.push(numbers[i]);
-    }
-  }
-  return newArray;
-}
+function getElementWidth(content, padding, border) {
+  const paddingValue = parseFloat(padding);
+  const borderValue = parseFloat(border);
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  const totalWidth = contentValue + paddingValue + borderValue;
+}
+return `${totalWidth}px`;
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
